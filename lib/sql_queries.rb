e96a41eaa195a6e1826405166a4cd6335f3c45c6
kristopherbullinger <github email address>
2018-09-26 17:28:13 -0400

    Lab Complete

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,29 +3,31 @@
 # def select_category_from_projects
 # "SELECT category FROM projects;"
 # end
-
+# pledges (id, amount, user_id, project_id)
+# projects (id, title, category, funding_goal, start_date, end_date)
+# users (id, name, age)
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"Write your SQL query Here"
+"select projects.title, SUM(pledges.amount) from projects join pledges on projects.id = pledges.project_id group by projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"Write your SQL query Here"
+"select users.name, users.age, SUM(pledges.amount) from users join pledges on users.id = pledges.user_id group by users.id order by users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"select projects.title, SUM(pledges.amount) - projects.funding_goal from projects join pledges on projects.id = pledges.project_id group by projects.id HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"select users.name, SUM(pledges.amount) from users join pledges on users.id = pledges.user_id group by users.name order by SUM(pledges.amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+'select projects.category, pledges.amount from pledges join projects on pledges.project_id = projects.id where projects.category = "music";'
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+'select projects.category, SUM(pledges.amount) from projects join pledges on projects.id = pledges.project_id where projects.category = "books" group by projects.category;'
 end
